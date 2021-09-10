557df3197c1f5a06e0a53358efbfede379387c7b
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-09-10 11:46:51 -0700

    algorithm design manual 3ed exercise 3.1

--- algorithm_design_manual_2ed/ch_03/1.c
+++ algorithm_design_manual_2ed/ch_03/1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "data_structures.h"
-#include "example.h"
 
 
 /* 
@@ -25,19 +25,60 @@ will be three cases:
 The string will contain unbalanced parens if the stack is not empty.
 */
 
-bool checkParens(char *stringToCheck) {
-    printf("%s\n", stringToCheck);
-    return true;
+
+struct parensInfo {
+    bool isValid;
+    int firstInvalidParensLocation;
+};
+struct parensInfo set(bool isValid, int firstInvalidParensLocation) {
+    struct parensInfo the_struct;
+    the_struct.isValid = isValid;
+    the_struct.firstInvalidParensLocation = firstInvalidParensLocation;
+    return the_struct;
+}
+struct parensInfo checkParens(char *stringToCheck) {
+    Stack* stack = createStack(100);
+    int stringLength = strlen(stringToCheck);
+    struct parensInfo returnValue;
+    for (int i = 0; i < stringLength; i++) {
+        char currChar = stringToCheck[i];
+        if (currChar == '(') {
+            push(stack, i);
+        }
+        if (currChar == ')') {
+            if (isEmpty(stack)) {
+                returnValue = set(false, i);
+                return returnValue;
+            }
+            else {
+                pop(stack);
+            }
+          
+        }
+    }
+    if (isEmpty(stack)) {
+        returnValue = set(true, 0);
+    }
+    else {
+        returnValue = set(false, 0);
+    }
+    
+    return returnValue;
 }
 int main()
 {
-    // char test_1[11] = "((())())()";
-    // char test_2[5] = ")()(";
-    // char test_3[4] = "())";
-    Stack* stack = createStack(100);
-    push(stack, 10);
-    push(stack, 20);
-    printf("%d popped from stack\n", pop(stack));
+    char test_1[11] = "((())())()";
+    char test_2[5] = ")()(";
+    char test_3[4] = "())";
+    char test_4[5] = "(())";
+    printf("%s: %d\n", test_1, checkParens(test_1).isValid);
+    printf("%s: %d\n", test_2, checkParens(test_2).isValid);
+    printf("%s: %d\n", test_3, checkParens(test_3).isValid);
+    printf("%s: %d\n", test_4, checkParens(test_4).isValid);
+    // Stack* stack = createStack(100);
+    // push(stack, 10);
+    // push(stack, 20);
+    // printf("%d popped from stack\n", pop(stack));
     // int myfoo = foo(4);
     // printf("%d", myfoo);
     return 0;
