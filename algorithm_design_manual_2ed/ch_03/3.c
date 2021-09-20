3a0b29f354f2882216bdfc432c181442ae556f9f
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-09-20 14:04:09 -0700

    WIP Refactor

--- algorithm_design_manual_2ed/ch_03/3.c
+++ algorithm_design_manual_2ed/ch_03/3.c
@@ -6,75 +6,21 @@
 
 /* 
 Stacks, Queues, and Lists
-3.3 Give an algorithm to reverse the direction of a given singly linked list. In
-other words, after the reversal all pointers should now point backwards. Your
-algorithm should take linear time.
+3.3 We have seen how dynamic arrays enable arrays to grow while still achieving
+constant-time amortized performance. This problem concerns extending dynamic
+arrays to let them both grow and shrink on demand.
+(a) Consider an underflow strategy that cuts the array size in half whenever the
+array falls below half full. Give an example sequence of insertions and deletions
+where this strategy gives a bad amortized cost.
+(b) Then, give a better underflow strategy than that suggested above, one that
+achieves constant amortized cost per deletion.
 */
-/* Iterative version. */
-/* 
-1. Initialize three pointers. prev and next are both NULL, while
-current is the pointer to the first item in the list (the head). 
-2. Iterate through the list. For every iteration do:
-    a. Store next node 
-    b. Change the next node to be instead the previous node
-    c. Set previous as current for the next iteration.
-    d. Set current to next for the next iteration.
-3. Set the new head of the list to be the element that was formerly last 
-in the list.
-*/
-void reverseSinglyLinkedList(List** list) {
 
-    Node *previous = NULL, *next = NULL, *current = (*list)->head;
- 
-    while (current != NULL) {
-        next = current->next;
-        current->next = previous;
-        previous = current;
-        current = next;
-    }
-    (*list)->head = previous;
-}
-
-/* Recursive Version */
-/* 
-1. Divide list into two parts, first node and rest of list 
-2. Call reverse for the rest of the linked list.
-3. Link rest of the list to the first of the list
-4. Fix head pointer
-*/
-Node *reverseSinglyLinkedListRecursive(Node** node) {
-    Node *next = (*node)->next;
-    if (next == NULL) {
-        return *node;
-    }
-    Node *rest = reverseSinglyLinkedListRecursive(&next);
-    (*node)->next->next = *node;
-    (*node)->next = NULL;
-    return rest;
-}
-void reverseSinglyLinkedListRecursiveEntry(List** list) {
-    Node *head = (*list)->head;
-    if (head != NULL) {
-        (*list)->head = reverseSinglyLinkedListRecursive(&head);
-    }
-}
 int main()
 {
-    List* list_1 = createList();
-    pushToList(&list_1, 9);
-    pushToList(&list_1, 8);
-    pushToList(&list_1, 7);
-    pushToList(&list_1, 6);
-    pushToList(&list_1, 5);
-    pushToList(&list_1, 4);
-    pushToList(&list_1, 3);
-    pushToList(&list_1, 2);
-    pushToList(&list_1, 1);
-    pushToList(&list_1, 0);
-    printList(list_1);
-    reverseSinglyLinkedListRecursiveEntry(&list_1);
-    printf("\n");
-    printList(list_1);
+    DynamicArray *my_array = create_DynamicArray();
+    append_DynamicArray(my_array, 5);
+    append_DynamicArray(my_array, 6);
     return 0;
 
 }
---
+++ algorithm_design_manual_2ed/ch_03/extra_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "data_structures.h"
+
+
+/* 
+NOTE: THIS IS 3.2 IN THE 3RD EDITION
+Stacks, Queues, and Lists 
+3.2 Give an algorithm that takes a string S consisting of opening and closing
+parentheses, say )()(())()()))())))(, and finds the length of the longest balanced
+parentheses in S, which is 12 in the example above. (Hint: The solution is not
+necessarily a contiguous run of parenthesis from S.)
+*/
+
+/* ------------------------------------------------ */
+/* 
+They are asking here for longest valid parentheses substring. 
+*/
+
+int main()
+{
+   
+    return 0;
+
+}
