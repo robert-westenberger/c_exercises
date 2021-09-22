efbfe25bc94fb5f37616b7e74a7a4e3cfcaf5679
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-09-22 10:55:18 -0700

    testing dynamic array

--- test/main.c
+++ test/main.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include "data_structures.h"
 
+void printCallback(int item) {
+    printf("%d \n", item);
+}
 int main()
 {
     DynamicArray *my_array = create_DynamicArray();
     append_DynamicArray(my_array, 1);
     append_DynamicArray(my_array, 2);
-    prepend_DynamicArray(my_array, 0);
     append_DynamicArray(my_array, 3);
     append_DynamicArray(my_array, 4);
     append_DynamicArray(my_array, 5);
@@ -16,28 +18,10 @@ int main()
     append_DynamicArray(my_array, 7);
     append_DynamicArray(my_array, 8);
     append_DynamicArray(my_array, 9);
-    
-    for (size_t i = 0; i < my_array->size; i++) {
-        printf("%d \n", *(my_array->items + i));
-        
-    }
-    printf("capacity: %d \n", my_array->capacity);
-    printf("size: %d \n", my_array->size);
-    // List* list_1 = createList();
-    // pushToList(&list_1, 9);
-    // pushToList(&list_1, 8);
-    // pushToList(&list_1, 7);
-    // pushToList(&list_1, 6);
-    // pushToList(&list_1, 5);
-    // pushToList(&list_1, 4);
-    // pushToList(&list_1, 3);
-    // pushToList(&list_1, 2);
-    // pushToList(&list_1, 1);
-    // pushToList(&list_1, 0);
-    // printList(list_1);
-    // printf("\n");
-    // deleteFromListByValue2(&list_1, 3);
-    // printList(list_1);
+    prepend_DynamicArray(my_array, 0);
+    forEach_DynamicArray(my_array, &printCallback);
+    printf("capacity %d \n", my_array->capacity);
+    printf("size %d \n", my_array->size);
     return 0;
 
 }
