e8261d0171c007a3849aa3c6862fa64399c55938
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-09-23 10:06:01 -0700

    Messing around with dynamic array

--- test/main.c
+++ test/main.c
@@ -6,22 +6,47 @@
 void printCallback(int item) {
     printf("%d \n", item);
 }
+
+void test(DynamicArray *my_array, char message[]) {
+    printf("START \n");
+    printf("%s \n", message);
+    printf("capacity %d \n", my_array->capacity);
+    printf("size %d \n", my_array->size);
+    printf("END \n");
+    printf("\n");
+}
 int main()
 {
     DynamicArray *my_array = create_DynamicArray();
-    append_DynamicArray(my_array, 1);
-    append_DynamicArray(my_array, 2);
-    append_DynamicArray(my_array, 3);
-    append_DynamicArray(my_array, 4);
-    append_DynamicArray(my_array, 5);
-    append_DynamicArray(my_array, 6);
-    append_DynamicArray(my_array, 7);
-    append_DynamicArray(my_array, 8);
-    append_DynamicArray(my_array, 9);
-    prepend_DynamicArray(my_array, 0);
-    forEach_DynamicArray(my_array, &printCallback);
-    printf("capacity %d \n", my_array->capacity);
-    printf("size %d \n", my_array->size);
+    // test(my_array, "Just created array");
+    // append_DynamicArray(my_array, 1);
+    // test(my_array, "Just appended 1 to array");
+    // append_DynamicArray(my_array, 2);
+    // test(my_array, "Just appended 2 to array");
+    // append_DynamicArray(my_array, 3);
+    // test(my_array, "Just appended 3 to array");
+    // append_DynamicArray(my_array, 4);
+    // test(my_array, "Just appended 4 to array");
+    // removeAtIndex_DynamicArray(my_array, 3);
+    // test(my_array, "Just removed 4 from array");
+    // removeAtIndex_DynamicArray(my_array, 2);
+    // test(my_array, "Just removed 3 from array");
+    // removeAtIndex2_DynamicArray(my_array, 1);
+    // test(my_array, "Just removed 2 from array");
+    // append_DynamicArray(my_array, 2);
+    // test(my_array, "Just appended 2 to array");
+    // append_DynamicArray(my_array, 2);
+    // test(my_array, "Just appended 2 to array");
+    // // append_DynamicArray(my_array, 5);
+    // // append_DynamicArray(my_array, 6);
+    // // append_DynamicArray(my_array, 7);
+    // // append_DynamicArray(my_array, 8);
+    // // append_DynamicArray(my_array, 9);
+    // // prepend_DynamicArray(my_array, 0);
+    // // removeAtIndex2_DynamicArray(my_array, 5);
+    // // forEach_DynamicArray(my_array, &printCallback);
+    // // printf("capacity %d \n", my_array->capacity);
+    // // printf("size %d \n", my_array->size);
     return 0;
 
 }
