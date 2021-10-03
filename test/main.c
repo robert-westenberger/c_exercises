bc9136661bcecd1cf63918d2a10838f55d8d6560
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-10-02 18:45:11 -0700

    BST algos

--- test/main.c
+++ test/main.c
@@ -10,17 +10,13 @@ int main()
     BSTNode* tree = NULL;
     insert_BinarySearchTree(&tree, 6);
     insert_BinarySearchTree(&tree, 5);
-    insert_BinarySearchTree(&tree, 5);
     insert_BinarySearchTree(&tree, 2);
+    insert_BinarySearchTree(&tree, 5);
     insert_BinarySearchTree(&tree, 7);
     insert_BinarySearchTree(&tree, 8);
-    // print_BinarySearchTree(tree, 0);
-    // height_BinarySearchTree(tree);
-    printf("%d \n", height_BinarySearchTree(tree));
-    // deleteValue_BinarySearchTree(tree, 27);
-    // printf("%d \n", minimum_BinarySearchTree(tree)->item);
-    // printf("%d \n", maximum_BinarySearchTree(tree)->item);
-    // printf("%d \n", search_BinarySearchTree(tree, 5)->item);
+    print_BinarySearchTree(tree, 0);
+    printf("\n");
+
     
     return 0;
 
