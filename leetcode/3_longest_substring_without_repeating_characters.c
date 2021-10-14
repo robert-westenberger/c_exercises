b2c235e96cf1edb0fade83cdf2adbc6051f438f3
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-10-14 11:50:01 -0700

    hash map testing

--- leetcode/3_longest_substring_without_repeating_characters.c
+++ leetcode/3_longest_substring_without_repeating_characters.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include "string_helpers.h"
 #include "map.h"
 
 /*
--- test/hash_map_testing.c
+++ test/hash_map_testing.c
@@ -2,16 +2,28 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include "string_helpers.h"
 #include "map.h"
+
+
+
+
 
 int main()
 {   
     char s1[] = "pwwkew";
     MapNode *hashMap[HASHSIZE] = { NULL };
+    char s2[] = "";
+   
+    substring(s2, &s1[1], 1);
     
-    set_HashMap(hashMap, "test", 5);
+    set_HashMap(hashMap, s2, 1);
+    printf("%d \n", (get_HashMap(hashMap, s2))->value);
+    substring(s2, &s1[2], 1);
+    set_HashMap(hashMap, s2, 2);
+    printf("%d \n", (get_HashMap(hashMap, s2))->value);
     
-    printf("%d \n", (get_HashMap(hashMap, "test"))->value);
+    
     return 0;
 
 }
