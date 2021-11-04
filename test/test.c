4710d84e3d7370de160a3f56948bce520f41a9a0
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-11-04 11:36:10 -0700

    WIP AVL implementation

--- test/test.c
+++ test/test.c
@@ -8,7 +8,7 @@ void preOrder(AVLTree* tree, AVLNode *node)
 {
   if (node != NULL)
   {
-    printf("%s\n", (const char *) node->data);
+    tree->data_ops.print(node->data);
     preOrder(tree, node->left);
     preOrder(tree, node->right);
   }
@@ -17,9 +17,23 @@ void preOrder(AVLTree* tree, AVLNode *node)
 int main()
 {
 
-  AVLTree* string_tree = create_AVLTree(data_ops_string);
-  string_tree->head = insert_AVLTree(string_tree, "Test");
-  string_tree->head = insert_AVLTree(string_tree, "Bar");
-  preOrder(string_tree, string_tree->head);
+  // AVLTree* string_tree = create_AVLTree(data_ops_string);
+  // string_tree->head = insert_AVLTree(string_tree, string_tree->head, "Test");
+  // string_tree->head = insert_AVLTree(string_tree, string_tree->head, "Bar");
+  // preOrder(string_tree, string_tree->head);
+  AVLTree *int_tree = create_AVLTree(data_ops_int);
+  int ten = 10;
+  int twenty = 20;
+  int thirty = 30;
+  int fourty = 40;
+  int fifty = 50;
+  int twentyfive = 25;
+  int_tree->head = insert_AVLTree(int_tree, int_tree->head, &ten);
+  int_tree->head = insert_AVLTree(int_tree, int_tree->head, &twenty);
+  int_tree->head = insert_AVLTree(int_tree, int_tree->head, &thirty);
+  int_tree->head = insert_AVLTree(int_tree, int_tree->head, &fourty);
+  int_tree->head = insert_AVLTree(int_tree, int_tree->head, &fifty);
+  int_tree->head = insert_AVLTree(int_tree, int_tree->head, &twentyfive);
+  preOrder(int_tree, int_tree->head);
   return 0;
 }
